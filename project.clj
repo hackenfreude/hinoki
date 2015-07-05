df2a92417db79fb240a33aecf3d6cffc8921a1fa
hackenfreude <hackenfreude@users.noreply.github.com>
2015-07-05 16:14:20 -0700

    add uuid

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.2"]
                  [ring/ring-jetty-adapter "1.3.2"]
-                 [clj-http "1.1.2"]]
+                 [clj-http "1.1.2"]
+                 [danlentz/clj-uuid "0.1.6"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
   :plugins [[lein-midje "3.1.3"]]
   :test-paths ["test"])
--- src/hinoki/core.clj
+++ src/hinoki/core.clj
@@ -1,14 +1,29 @@
 (ns hinoki.core
   (:require [clj-http.client :as client]
-            [ring.util.response :use get-header]))
+            [ring.util.response :use get-header]
+            [clj-uuid :as uuid]))
+
+(defn get-trace [request]
+  (get-header request "trace"))
+
+(defn get-span [request]
+  (get-header request "span"))
+
+(defn get-parent [request]
+  (get-header request "parent"))
 
 (defn get-correlations [request]
-  (hash-map :trace (get-header request "trace") :span (get-header request "span") :parent (get-header request "parent")))
+  (hash-map :trace (get-trace request) :span (get-span request) :parent (get-parent request)))
+
+(defn log-entry [request caller]
+  (println "BEGIN: " caller (get-trace request)))
 
 (defn first-handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (str (get-correlations request))})
+  (do
+    (log-entry request "first-handler")
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (str (get-correlations request))}))
 
 (defn second-handler [request]
   {:status 200
@@ -16,7 +31,7 @@
    :body "2 two"})
 
 (defn make-correlation []
-  (hash-map :trace "me" :span "you" :parent "them"))
+  (hash-map :trace (uuid/v1) :span (uuid/v1) :parent (uuid/null)))
 
 (defn begin []
   (client/get "http://localhost:11111"
